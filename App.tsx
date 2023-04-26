1249ee1197538a16cdcf366b55193b856a9fcee0
abhicoder22 <abhishekpal.ap98@gmail.com>
2023-04-26 18:47:35 +0530

    CreateScreenAdded

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import {Image, TouchableOpacity} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import IndexScreen from './src/Screens/IndexScreen';
 import {Provider} from './src/Context/BlogContext';
+import ShowScreen from './src/Screens/showScreen';
+import CreateScreen from './src/Screens/CreateScreen';
 
 const stack = createNativeStackNavigator();
 
@@ -10,7 +13,22 @@ const App = () => {
   return (
     <NavigationContainer>
       <stack.Navigator>
-        <stack.Screen name="Index" component={IndexScreen} />
+        <stack.Screen
+          name="Index"
+          component={IndexScreen}
+          options={({navigation}) => ({
+            headerRight: () => (
+              <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+                <Image
+                  style={{height: 30, width: 30}}
+                  source={require('./src/Assets/plus.png')}
+                />
+              </TouchableOpacity>
+            ),
+          })}
+        />
+        <stack.Screen name="Show" component={ShowScreen} />
+        <stack.Screen name="Create" component={CreateScreen} />
       </stack.Navigator>
     </NavigationContainer>
   );
--- src/Context/BlogContext.js
+++ src/Context/BlogContext.js
@@ -21,21 +21,43 @@ import createDataContext from './createDataContext';
 
 const BlogReducer = (state, action) => {
   switch (action.type) {
+    case 'delete_blogpost':
+      return state.filter(BlogPost => BlogPost.id !== action.payload);
     case 'add_blogpost':
-      return [...state, {title: `Blog post #${state.length + 1}`}];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          // title: `Blog Post #${state.length + 1}`,
+          title: action.payload.title,
+          content: action.payload.content,
+        },
+      ];
     default:
       return state;
   }
 };
 
 const addBlogPosts = dispatch => {
-  return () => {
-    dispatch({type: 'add_blogpost'});
+  return (title, content) => {
+    dispatch({
+      type: 'add_blogpost',
+      payload: {title, content},
+    });
+  };
+};
+
+const deleteBlogPost = dispatch => {
+  return id => {
+    dispatch({
+      type: 'delete_blogpost',
+      payload: id,
+    });
   };
 };
 
 export const {Context, Provider} = createDataContext(
   BlogReducer,
-  {addBlogPosts},
+  {addBlogPosts, deleteBlogPost},
   [],
 );
